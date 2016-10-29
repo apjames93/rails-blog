a5ac006fa3ca054e9c35e8c0f6c0ec9d8b4fd355
Alexander James <apjames93@gmail.com>
2016-10-29 13:01:03 -0600

    made create route

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,7 +2,17 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  # the create route for the create action and passing through the pricate article_params to insure they are of a specific type
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
+  #  private function to send params
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
+# 5.6
