679fdb0ad1aeabfb9ff6cd65cffb2e6042e7bfbb
Alexander James <apjames93@gmail.com>
2016-10-29 13:37:40 -0600

    show in ruby

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,18 +1,23 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+   @article = Article.find(params[:id])
+  end
+
   def new
   end
 
-  # the create route for the create action and passing through the pricate article_params to insure they are of a specific type
   def create
     @article = Article.new(article_params)
-
     @article.save
     redirect_to @article
   end
-  #  private function to send params
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-end
-# 5.6
+  end
